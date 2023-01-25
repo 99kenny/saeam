3a21006a116821345e3b8f46753bbb13740005ea
99kenny <helloiamkenny@gmail.com>
2023-01-25 13:43:34 +0900

    update

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import collections
-import collections.abc
-from pptx import Presentation
-from pptx.util import Pt
-from pptx.enum.text import PP_PARAGRAPH_ALIGNMENT
-from pptx.dml.color import RGBColor
-import sys
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+
 from ui import *
-
-
-
-
-
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = App()
+
     sys.exit(app.exec_())
 
 
--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 import sys
+
+from PyQt5.QtCore import QThread, QObject, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QListWidget, QVBoxLayout, QLineEdit, QTextEdit, QLabel, \
     QPushButton, QScrollArea
 import collections
 import collections.abc
 from pptx import Presentation
-from pptx.util import Pt
+from pptx.util import Pt, Cm
 from pptx.enum.text import PP_PARAGRAPH_ALIGNMENT
 from pptx.dml.color import RGBColor
 import sys
@@ -16,8 +18,17 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from ui import *
 
+def trap_exc_during_debug(*args):
+    # when app raises uncaught exception, print info
+    print(args)
+
+
+# install exception hook: without this, uncaught exception would cause application to exit
+sys.excepthook = trap_exc_during_debug
 
 class App(QWidget):
+    sig_start = pyqtSignal()  # needed only due to PyCharm debugger bug (!)
+    sig_abort_workers = pyqtSignal()
     def __init__(self):
         super().__init__()
         self.files = []
@@ -30,9 +41,8 @@ class App(QWidget):
     def initUI(self):
         self.layout = QVBoxLayout()
         self.setWindowTitle("최새암 바보")
-        self.move(300,300)
-        self.resize(1000,600)
-
+        self.move(300, 300)
+        self.resize(1000, 600)
 
         lay = QVBoxLayout()
         self.button = QPushButton("확인")
@@ -41,7 +51,6 @@ class App(QWidget):
         self.list.setLayout(self.layout)
 
         self.setAcceptDrops(True)
-
 
         lay.addWidget(self.list)
         lay.addWidget(self.button)
@@ -111,15 +120,21 @@ class App(QWidget):
             month = seperated[0][4:6].strip()
             day = seperated[0][6:].strip()
             contents = seperated[1].strip()
-            temp = seperated[2].strip().split(" ")    # 요 6_61~63, 계 2 fix here
+            temp = seperated[2].strip().split(",")  # 요6_61~63, 계2_2~3 fix here
 
+            what = []
+            for i in range(len(temp)):
+                if "~" in temp[i]:
+                    what.append([bible_abrv[temp[i][0]], temp[i][1], temp[i].split("_")[1].split("~")[0],
+                                 temp[i].split("_")[1].split("~")[1]])
+                else:
+                    what.append([bible_abrv[temp[i][0]], temp[i][1], temp[i].split("_")[1]])
 
-            words = bible_abrv[temp[0].strip()]
-            chapter = temp[1].strip()
-            clause = temp[2].strip()
+            # words = bible_abrv[temp[0].strip()]
+            # chapter = temp[1].strip()
+            # clause = temp[2].strip()
 
-
-            pastor = seperated[3].strip()
+            pastor = seperated[3].strip().split(".")[0]
 
             # pptx config
             fpath = "./template.pptx"
@@ -134,10 +149,16 @@ class App(QWidget):
 
             self.text_on_shape(shape_list[shape_index["title"]], pastor + " 설교", RGBColor(255, 255, 255), font_size=40)
             self.text_on_shape(shape_list[shape_index["date"]], year + "년 " + month + "월 " + day + "일",
-                          RGBColor(181, 251, 5))
+                               RGBColor(181, 251, 5))
             self.text_on_shape(shape_list[shape_index["content"]], "'" + contents + "'", RGBColor(255, 255, 255))
-            self.text_on_shape(shape_list[shape_index["words"]], words + " " + chapter + "장 " + clause + "절",
-                          RGBColor(181, 251, 5))
+            result = ""
+            for i in range(len(what)):
+                if len(what[i]) == 3:
+                    result += (what[i][0] + " " + what[i][1] + "장 " + what[i][2] + "절 ")
+                else:
+                    result += (what[i][0] + " " + what[i][1] + "장 " + what[i][2] + "~" + what[i][3] + "절 ")
+
+            self.text_on_shape(shape_list[shape_index["words"]], result, RGBColor(181, 251, 5))
 
             # slide 2
             slide = prs.slides[1]
@@ -147,48 +168,68 @@ class App(QWidget):
                 shape_index[shape.name] = i
 
             self.text_on_shape(shape_list[shape_index["title"]], "'" + contents + "' ", RGBColor(255, 255, 255),
-                          font_size=28)
-            self.text_on_shape(shape_list[shape_index["title"]], words + " " + chapter + "장 " + clause + "절",
-                          RGBColor(181, 251, 5), font_size=28, clear=False)
+                               font_size=28)
+            self.text_on_shape(shape_list[shape_index["title"]], result, RGBColor(181, 251, 5), font_size=28,
+                               clear=False)
 
+            gae = []
+            saenew = []
             # 개역개정
-            driver.get(url)
-            select = Select(driver.find_elements(By.NAME, "VR")[0])
-            select.select_by_value('GAE')
-            search = driver.find_element(By.NAME, "QR")
-            search.send_keys(words + chapter + "장")
-            submit = driver.find_element(By.CSS_SELECTOR,
-                                         "body > table:nth-child(3) > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > font.tk2 > input[type=submit]:nth-child(2)")
-            submit.click()
+            for i in range(len(what)):
+                driver.get(url)
+                select = Select(driver.find_elements(By.NAME, "VR")[0])
+                select.select_by_value('GAE')
+                search = driver.find_element(By.NAME, "QR")
+                search.send_keys(what[i][0] + what[i][1] + "장")
+                submit = driver.find_element(By.CSS_SELECTOR,
+                                             "body > table:nth-child(3) > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > font.tk2 > input[type=submit]:nth-child(2)")
+                submit.click()
 
-            index = ((int(clause) - 1) // TABLE_SIZE)
-            list_index = ((int(clause) - 1) % TABLE_SIZE)
-            num = index * TABLE_SIZE + 1
-            clause_id = "b_" + "{0:03d}".format(num)
-            ol = driver.find_element(By.ID, clause_id)
+                if len(what[i]) == 3:
+                    clauses = [what[i][2]]
+                else:
+                    clauses = range(int(what[i][2]), int(what[i][3]) + 1)
 
-            gae = ol.find_elements(By.TAG_NAME, "li")[list_index].find_element(By.TAG_NAME, "font").text
+                for clause in clauses:
+                    index = ((int(clause) - 1) // TABLE_SIZE)
+                    list_index = ((int(clause) - 1) % TABLE_SIZE)
+                    num = index * TABLE_SIZE + 1
+                    clause_id = "b_" + "{0:03d}".format(num)
 
-            # 새번역
-            driver.get(url)
-            select = Select(driver.find_elements(By.NAME, "VR")[0])
-            select.select_by_value('SAENEW')
-            search = driver.find_element(By.NAME, "QR")
-            search.send_keys(words + chapter + "장")
-            submit = driver.find_element(By.CSS_SELECTOR,
-                                         "body > table:nth-child(3) > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > font.tk2 > input[type=submit]:nth-child(2)")
-            submit.click()
+                    ol = driver.find_element(By.ID, clause_id)
+                    gae.append(ol.find_elements(By.TAG_NAME, "li")[list_index].find_element(By.TAG_NAME, "font").text)
+                # 새번역
+                driver.get(url)
+                select = Select(driver.find_elements(By.NAME, "VR")[0])
+                select.select_by_value('SAENEW')
+                search = driver.find_element(By.NAME, "QR")
+                search.send_keys(what[i][0] + what[i][1] + "장")
+                submit = driver.find_element(By.CSS_SELECTOR,
+                                             "body > table:nth-child(3) > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td > font.tk2 > input[type=submit]:nth-child(2)")
+                submit.click()
 
-            index = ((int(clause) - 1) // TABLE_SIZE)
-            list_index = ((int(clause) - 1) % TABLE_SIZE)
-            num = index * TABLE_SIZE + 1
-            clause_id = "b_" + "{0:03d}".format(num)
-            ol = driver.find_element(By.ID, clause_id)
+                for clause in clauses:
+                    index = ((int(clause) - 1) // TABLE_SIZE)
+                    list_index = ((int(clause) - 1) % TABLE_SIZE)
+                    num = index * TABLE_SIZE + 1
+                    clause_id = "b_" + "{0:03d}".format(num)
+                    ol = driver.find_element(By.ID, clause_id)
+                    saenew.append(
+                        ol.find_elements(By.TAG_NAME, "li")[list_index].find_element(By.TAG_NAME, "font").text)
 
-            saenew = ol.find_elements(By.TAG_NAME, "li")[list_index].find_element(By.TAG_NAME, "font").text
-
-            self.text_on_shape(shape_list[shape_index["new_translation"]], saenew, RGBColor(255, 255, 255))
-            self.text_on_shape(shape_list[shape_index["revision"]], gae, RGBColor(204, 255, 204))
+            left = 2.7
+            top = 3.41
+            width = 30
+            height = 2.86
+            for k in range(len(saenew)):
+                tb1 = slide.shapes.add_textbox(Cm(left), Cm(top + (2 * k * height)), Cm(width), Cm(height))
+                tb2 = slide.shapes.add_textbox(Cm(left), Cm(top + (2 * k * height) + height), Cm(width), Cm(height))
+                tf1 = tb1.text_frame
+                tf2 = tb2.text_frame
+                tf1.word_wrap = True
+                tf2.word_wrap = True
+                self.text_on_shape(tb1, saenew[k], RGBColor(255, 255, 255), font_size=28)
+                self.text_on_shape(tb2, gae[k], RGBColor(204, 255, 204), font_size=28)
 
             # slide 3
             slide = prs.slides[2]
@@ -198,20 +239,32 @@ class App(QWidget):
                 shape_index[shape.name] = i
 
             self.text_on_shape(shape_list[shape_index["title"]], "'" + contents + "' ", RGBColor(255, 255, 255),
-                          font_size=28)
-            self.text_on_shape(shape_list[shape_index["title"]], words + " " + chapter + "장 " + clause + "절",
-                          RGBColor(181, 251, 5), font_size=28, clear=False)
-            self.text_on_shape(shape_list[shape_index["new_translation"]], saenew, RGBColor(255, 255, 255))
-            self.text_on_shape(shape_list[shape_index["revision"]], gae, RGBColor(204, 255, 204))
+                               font_size=28)
+            self.text_on_shape(shape_list[shape_index["title"]], result,
+                               RGBColor(181, 251, 5), font_size=28, clear=False)
+
+            for k in range(len(saenew)):
+                tb1 = slide.shapes.add_textbox(Cm(left), Cm(top + (2 * k * height)), Cm(width), Cm(height))
+                tb2 = slide.shapes.add_textbox(Cm(left), Cm(top + (2 * k * height) + height), Cm(width), Cm(height))
+                tf1 = tb1.text_frame
+                tf2 = tb2.text_frame
+                tf1.word_wrap = True
+                tf2.word_wrap = True
+                self.text_on_shape(tb1, saenew[k], RGBColor(255, 255, 255), font_size=28)
+                self.text_on_shape(tb2, gae[k], RGBColor(204, 255, 204), font_size=28)
 
             prs.save(seperated[0] + seperated[1] + seperated[2] + seperated[3] + ".pptx")
 
         driver.quit()
+
     def clicked(self):
         self.make_ppt(self.files)
         self.files.clear()
         for i in range(self.layout.count()):
             self.layout.itemAt(i).widget().deleteLater()
+
+        print("working")
+
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
             event.accept()
